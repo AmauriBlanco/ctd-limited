29dda84de45031b64e54b9fc01d920025e8c7482
AmauriBlanco <blancoamauri@gmail.com>
2023-04-12 22:14:36 -0300

    atributos de acessibilidade

--- assets/mailer.php
+++ assets/mailer.php
@@ -21,10 +21,10 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "amandrd05@gmail.com";
+        $recipient = "ctdunlimited@gmail.com";
 
         // Set the email subject.
-        $subject = "New contact from Applan";
+        $subject = "New contact from CTD Web site";
 
         // Build the email content.
         $email_content = "Name: $name\n";
